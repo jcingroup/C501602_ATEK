e469eedf415e361f5adb525e87bebefa9c7d71c6
TinWu <yyuuix@gmail.com>
2016-01-27 16:14:31 +0800

    update

--- Work.WebProj/Content/js/common.js
+++ Work.WebProj/Content/js/common.js
@@ -8,7 +8,7 @@ $(window).scroll(function(){
 });
 
 // 點選後跳到 href 指向的位置
-$('.goTop').click(function () {
+$('.scroll, .scroll a').click(function () {
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 750);
@@ -29,11 +29,11 @@ $(document).ready(function() {
     });
 
     // 行動裝置的產品分類選單
-    $(".pro-menu").click(function() {
-        $(this).toggleClass("active");
-        // $('aside nav').slideToggle(750);
-        $('aside nav').toggleClass('open');
-    });
+    // $(".pro-menu").click(function() {
+    //     $(this).toggleClass("active");
+    //     // $('aside nav').slideToggle(750);
+    //     $('aside nav').toggleClass('open');
+    // });
 
     // 下拉選單
     $(".dropbtn").click(function(){
