9d31dd132a0f00deb52713f3c183b23c4522c4f6
Ajoe <sdec0817@gmail.com>
2016-01-28 16:06:35 +0800

    後台增加 aboutusData相關檔案

--- Work.WebProj/webpack.config.js
+++ Work.WebProj/webpack.config.js
@@ -10,33 +10,11 @@ module.exports = {
         m_change_password: path.resolve(__dirname, 'Scripts/src/tsx/m-change_password.js'),
         m_users: path.resolve(__dirname, 'Scripts/src/tsx/m-users.js'),
         //後台 管理者
-        m_sales: path.resolve(__dirname, 'Scripts/src/tsx/m-sales.js'),
-        m_sales_rank: path.resolve(__dirname, 'Scripts/src/tsx/m-sales_rank.js'),
-        m_product: path.resolve(__dirname, 'Scripts/src/tsx/m-product.js'),
-        m_purchase: path.resolve(__dirname, 'Scripts/src/tsx/m-purchase.js'),
-        m_settle: path.resolve(__dirname, 'Scripts/src/tsx/m-settle.js'),
         m_news: path.resolve(__dirname, 'Scripts/src/tsx/m-news.js'),
         m_issue: path.resolve(__dirname, 'Scripts/src/tsx/m-issue.js'),
-        m_product_category_l1: path.resolve(__dirname, 'Scripts/src/tsx/m-product_category_l1.js'),
-        m_product_category_l2: path.resolve(__dirname, 'Scripts/src/tsx/m-product_category_l2.js'),
-        m_product_set: path.resolve(__dirname, 'Scripts/src/tsx/m-product_set.js'),
         m_aboutus: path.resolve(__dirname, 'Scripts/src/tsx/m-aboutus.js'),
         m_parm: path.resolve(__dirname, 'Scripts/src/tsx/m-parm.js'),
-        m_payment_reply: path.resolve(__dirname, 'Scripts/src/tsx/m-payment_reply.js'),
         m_banner: path.resolve(__dirname, 'Scripts/src/tsx/m-banner.js'),
-        m_settle_accumulate: path.resolve(__dirname, 'Scripts/src/tsx/m-settle_accumulate.js'),
-        //前台
-        w_purchase_cart: path.resolve(__dirname, 'Scripts/src/tsx/w-purchase_cart.js'),
-        w_purchase_history: path.resolve(__dirname, 'Scripts/src/tsx/w-purchase_history.js'),
-        w_payment_order: path.resolve(__dirname, 'Scripts/src/tsx/w-payment_order.js'),
-        w_payment_reply: path.resolve(__dirname, 'Scripts/src/tsx/w-payment_reply.js'),
-        w_product_search: path.resolve(__dirname, 'Scripts/src/tsx/w-product_search.js'),
-        //後台 一般會員
-        m_sales_self: path.resolve(__dirname, 'Scripts/src/tsx/m-sales_self.js'),
-        m_settle_self: path.resolve(__dirname, 'Scripts/src/tsx/m-settle_self.js'),
-        m_purchase_view: path.resolve(__dirname, 'Scripts/src/tsx/m-purchase_view.js'),
-        m_share_circle: path.resolve(__dirname, 'Scripts/src/tsx/m-share_circle.js'),
-        m_sales_recommend: path.resolve(__dirname, 'Scripts/src/tsx/m-sales_recommend.js'),
         vendors: ['jquery', 'react', 'react-bootstrap', 'moment']
     },
     output: {
