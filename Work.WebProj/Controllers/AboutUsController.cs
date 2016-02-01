31ef9570569aad152f2f18997389637d5ff8d325
Ajoe <sdec0817@gmail.com>
2016-02-01 10:24:32 +0800

    關於我們前台套程式

--- Work.WebProj/Controllers/AboutUsController.cs
+++ Work.WebProj/Controllers/AboutUsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DotWeb.Controller;
+using ProcCore.Business.DB0;
 
 namespace DotWeb.WebApp.Controllers
 {
@@ -12,7 +13,19 @@ namespace DotWeb.WebApp.Controllers
         // GET: AboutUs
         public ActionResult Index()
         {
-            return View("AboutUs");
+            List<m_AboutUsDetail> detail = new List<m_AboutUsDetail>();
+            using (var db0 = getDB0())
+            {
+                #region banner
+                detail = db0.AboutUsDetail.Where(x => !x.i_Hide & x.i_Lang == System.Globalization.CultureInfo.CurrentCulture.Name).OrderBy(x => x.sort)
+                                         .Select(x => new m_AboutUsDetail()
+                                         {
+                                             aboutus_detail_id = x.aboutus_detail_id,
+                                             detail_content = x.detail_content
+                                         }).ToList();
+                #endregion
+            }
+            return View("AboutUs", detail);
         }
     }
 }
