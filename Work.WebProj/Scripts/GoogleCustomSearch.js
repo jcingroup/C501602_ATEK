7be8b90c303a1ac594a5241818b36d261ca58751
Ajoe <sdec0817@gmail.com>
2016-03-03 17:32:31 +0800

    先改回來

--- Work.WebProj/Scripts/GoogleCustomSearch.js
+++ Work.WebProj/Scripts/GoogleCustomSearch.js
@@ -1,11 +1,21 @@
-﻿(function () {
+﻿function gcseCallback() {
+    if (document.readyState != 'complete')
+        return google.setOnLoadCallback(gcseCallback, true);
+    google.search.cse.element.render({ gname: 'gsearch', div: 'results', tag: 'searchresults-only', attributes: { linkTarget: '' } });
+    var element = google.search.cse.element.getElement('gsearch');
+    element.execute('this is my query');
+};
+window.__gcse = {
+    parsetags: 'explicit',
+    callback: gcseCallback
+};
+(function () {
     var cx = '006882016541431297896:jhlz5vusrik';
     var gcse = document.createElement('script');
     gcse.type = 'text/javascript';
     gcse.async = true;
     gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-        '//cse.google.com/cse.js?cx=' + cx;
+      '//www.google.com/cse/cse.js?cx=' + cx;
     var s = document.getElementsByTagName('script')[0];
-    console.log(s);
     s.parentNode.insertBefore(gcse, s);
 })();
