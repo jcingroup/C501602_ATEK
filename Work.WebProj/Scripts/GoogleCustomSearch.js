fde928bc56a86c0a2ae4f687ca40620a3aff679d
Ajoe <sdec0817@gmail.com>
2016-03-03 17:05:32 +0800

    + google customer search

---
+++ Work.WebProj/Scripts/GoogleCustomSearch.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    var cx = '006882016541431297896:jhlz5vusrik';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    console.log(s);
+    s.parentNode.insertBefore(gcse, s);
+})();
